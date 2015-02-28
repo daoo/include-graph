03bd887785003af8730c22a7a4194e7de678c151
Daniel Oom <daoo314@gmail.com>
2015-02-28 13:14:16 +0100

    Clean up and add Haskell support

--- IncludeGraph.hs
+++ IncludeGraph.hs
@@ -2,32 +2,52 @@
 module Main (main) where
 
 import Control.Applicative
+import Control.Monad
 import Data.Attoparsec.ByteString.Char8
+import Data.Bifunctor
 import Data.ByteString.Builder hiding (word8)
+import Data.Either
 import Data.List
-import Data.Maybe
 import Data.Monoid
 import System.IO
 import System.Posix.FilePath
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 
-include :: Parser RawFilePath
-include = string "#include" *> skipSpace *> (system <|> local)
+-- TODO: Fully qualify all paths
+
+includeCPP :: Parser RawFilePath
+includeCPP = string "#include" *> skipSpace *> (system <|> local)
   where
     system = between '<' '>'
     local  = between '"' '"'
 
     between a b = char a *> takeWhile1 (/= b)
 
-includes :: [B.ByteString] -> [RawFilePath]
-includes = mapMaybe (mby . parseOnly include)
+includeHaskell :: Parser RawFilePath
+includeHaskell = string "import" *> skipSpace *> mby (string "qualified") *> (topath <$> modref)
   where
-    mby (Right a) = Just a
-    mby _         = Nothing
+    mby = option () . void
 
-type File     = RawFilePath
-type Includes = (File, [File])
+    modref = takeWhile1 modchar
+
+    modchar c = isAlpha_ascii c || isDigit c || c == '.'
+
+    topath = (`B.append` ".hs") . B.map f
+      where
+        f '.' = '/'
+        f x   = x
+
+-- TODO: Check file type
+includes :: (RawFilePath, [RawFilePath]) -> Includes
+includes = id `bimap` (rights . map (parseOnly incl))
+  where
+    incl = includeCPP <|> includeHaskell
+
+fromFile :: RawFilePath -> IO Includes
+fromFile path = includes . ((,) path . B.lines) <$> B.readFile (B.unpack path)
+
+type Includes = (RawFilePath, [RawFilePath])
 
 graph :: [Includes] -> Builder
 graph xs = "digraph includes {" <> nodes (groups xs) <> edges xs <> "}"
@@ -56,8 +76,11 @@ graph xs = "digraph includes {" <> nodes (groups xs) <> edges xs <> "}"
         f '-' = '_'
         f x   = x
 
-fromFile :: RawFilePath -> IO Includes
-fromFile f = ((,) f . includes . B.lines) <$> B.readFile (B.unpack f)
-
 main :: IO ()
-main = B.lines <$> B.getContents >>= mapM fromFile >>= hPutBuilder stdout . graph
+main = prog
+  where
+    prog = input >>= readfiles >>= output . graph
+
+    input     = B.lines <$> B.getContents
+    readfiles = mapM fromFile
+    output    = hPutBuilder stdout
